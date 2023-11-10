f61c65022c74f052285b3f3209876b3c84c5ceb0
isobelfc <74111831+isobelfc@users.noreply.github.com>
2023-11-10 18:14:47 +0000

    add description

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_function" "lambda" {
-  filename      = "lambda_payload.zip"
   function_name = var.lambda_name
+  description   = "A simple example lambda"
+  filename      = "lambda_payload.zip"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   timeout       = 15
