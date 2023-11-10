cad1d8b0b0c49d9b316ca45ab94db4151d9b6cc5
isobelfc <74111831+isobelfc@users.noreply.github.com>
2023-11-10 18:06:07 +0000

    allow lambda to write logs

--- iam.tf
+++ iam.tf
@@ -15,3 +15,12 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "lambda-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+data "aws_iam_policy" "lambda_basic_execution_policy" {
+  name = "AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = data.aws_iam_policy.lambda_basic_execution_policy.arn
+}
--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "lambda" {
-  filename          = "lambda_payload.zip"
-  function_name     = "example-lambda-function"
-  role              = aws_iam_role.iam_for_lambda.arn
-  handler           = "lambda_function.lambda_handler"
-  timeout           = 15
+  filename      = "lambda_payload.zip"
+  function_name = "example-lambda-function"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+  timeout       = 15
 
   runtime = "python3.11"
 }
