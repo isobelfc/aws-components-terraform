6557073204757ee4dd21a68b591d12f0788e4e91
isobelfc <74111831+isobelfc@users.noreply.github.com>
2023-11-13 17:29:04 +0000

    use libraries in lambda code

--- module/lambda.tf
+++ module/lambda.tf
@@ -4,10 +4,11 @@ resource "aws_lambda_function" "lambda" {
 
   s3_bucket         = "isobel-lambda-function-store"
   s3_key            = aws_s3_object.lambda_payload.key
+  s3_object_version = aws_s3_object.lambda_payload.version_id
 
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function.lambda_handler"
-  timeout       = 15
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda_function.lambda_handler"
+  timeout = 15
 
   runtime = "python3.11"
 }
--- module/lib/lambda_function.py
+++ module/lib/lambda_function.py
@@ -1,2 +1,16 @@
+import random
+from datetime import datetime
+import requests
+
+
 def lambda_handler(event, context):
-    return "Hello"
+    now = datetime.now()
+    month = now.month
+    day = now.day
+
+    url = f"https://byabbe.se/on-this-day/{month}/{day}/events.json"
+
+    response = requests.get(url).json()
+    event = random.choice(response["events"])
+
+    return f"On this day in {event['year']}: {event['description']}"
